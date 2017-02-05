41da2b63010647baad70901092e51a5a0217d1a2
lakhaNamdhari <polizic@yahoo.co.in>
2017-02-05 19:13:07 +0530

    - Added examples for Destructuring assignments, Object literals inheritance and Promises

--- main.js
+++ main.js
@@ -13,5 +13,41 @@
       document.addEventListener('click', () => this.greetUser());
     }
   };
-  obj.init();
+
+  //obj.init();
+  let person = {
+    name: 'lakha',
+    age: 28
+  };
+
+  // Destructuring assignments
+  let testDestructuring = () => {
+    let {name, age} = person;
+    console.log(`${name} is ${age} years old.`);
+  };
+  testDestructuring();
+
+  // Inheritance in Object literals
+  let testObjectLiterals = () => {
+    let developer = {
+      __proto__: person,
+      skill: 'javascript',
+      testDestructuring
+    };
+    console.log(developer);
+    developer.age = 20;
+    console.log(person);
+    person.age = 50;
+    console.log(developer);
+  };
+  testObjectLiterals();
+
+  // The new Promises
+  let testPromises = () => {
+    var pObj = new Promise((accept, reject) => {
+      setTimeout(() => accept(), 1000);
+    });
+    pObj.then( () => alert('Promise resolved!'));
+  };
+  testPromises();
 })();
